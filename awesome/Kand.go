7ac068f01275681ecffdca6f3a0072259a88a3af
Sakthivel Balasubramaniam <sakthivel.b@thoughtworks.com>
2020-03-09 22:45:29 +0530

    modify kand

--- awesome/Kand.go
+++ awesome/Kand.go
@@ -5,6 +5,8 @@ import "fmt"
 // User defined function type
 type math func(a int, b int) int
 
+type sample struct{}
+
 func Kand() {
 	// Anonymous function
 	anonFn := func() {
@@ -17,9 +19,13 @@ func Kand() {
 		fmt.Println("Anonymous function - 2")
 	}()
 
-	var add math = func(a int, b int) int {
-		return a + b
-	}
+	var num bool
+	println("num=", num)
+
+	var add math
+
+	add = returnAddFunc()
+
 	fmt.Println("sum=", add(5, 6))
 
 	var prod math = func(a int, b int) int {
@@ -27,10 +33,24 @@ func Kand() {
 	}
 	fmt.Println("prod=", prod(5, 6))
 
+	userDefinedFuncHandler(10, 5, add)
+	userDefinedFuncHandler(10, 5, prod)
+
 	userDefinedFuncHandler(10, 11, func(a int, b int) int {
 		return a - b
 	})
 
+}
+
+func dummy() (int, string, float32, error) {
+	return 1, "", 1.0, nil
+}
+
+func returnAddFunc() math {
+
+	return func(a int, b int) int {
+		return a + b
+	}
 }
 
 // High order function
