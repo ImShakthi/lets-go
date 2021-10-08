620f14b5788e0abc7e2fd08b937c9f07e197d2a6
Sakthivel B <ideamatex@gmail.com>
2021-10-08 23:46:49 +0530

    add base for playing in json encoder

---
+++ snippets/json_encoder.go
@@ -0,0 +1,35 @@
+package snippets
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func JsonEncoder() {
+	r := request{
+		Name: "test name",
+		Headers: []header{
+			{
+				Key:          "key1",
+				Value:        []byte("value 1"),
+			},
+		},
+	}
+
+	b, err := json.Marshal(r)
+	if err != nil {
+		fmt.Println(">>>", err)
+		return
+	}
+	fmt.Printf(">>request=%+v", string(b))
+}
+
+type request struct {
+	Name    string   `json:"name"`
+	Headers []header `json:"headers"`
+}
+
+type header struct {
+	Key          string   `json:"key"`
+	Value        []byte   `json:"value"`
+}
